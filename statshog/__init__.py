9cf60c696a1c2af5a039f4a5882b3fcebe40c9a6
macobo <macobo@users.noreply.github.com>
2021-05-05 13:58:23 +0000

    Bump version to 1.0.5

--- statshog/__init__.py
+++ statshog/__init__.py
@@ -5,6 +5,6 @@ from .client import TCPStatsClient
 from .client import UnixSocketStatsClient
 
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 VERSION = [int(x) for x in __version__.split('.')]
 __all__ = ["StatsClient", "TCPStatsClient", "UnixSocketStatsClient"]
